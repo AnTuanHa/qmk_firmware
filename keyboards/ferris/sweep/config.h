b0e47c5b79bac583ced957dabecf8e1345291bbb
An Ha <zero579911@gmail.com>
2021-11-25 23:54:50 -0500

    ferris/sweep: increase DEBOUNCE from 5 to 15
    Tried DEBOUNCE 0, 5, 10, and 15. 15 seems to have removed the chatter

--- keyboards/ferris/sweep/config.h
+++ keyboards/ferris/sweep/config.h
@@ -62,7 +62,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define UNUSED_PINS
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 5
+#define DEBOUNCE 15
 
 /* Serial settings */
 #define USE_SERIAL
