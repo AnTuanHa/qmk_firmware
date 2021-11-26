01a10c9477e5d21c0b863ff57bd913350cc17072
An Ha <zero579911@gmail.com>
2021-11-25 23:13:37 -0500

    ferris/sweep: add LTO_ENABLE flag
    Enables Link Time Optimization (LTO) when compiling the keyboard.  This
    makes the process take longer, but it can significantly reduce the
    compiled size (and since the firmware is small, the added time is not
    noticeable).
    
    However, this will automatically disable the legacy TMK Macros and
    Functions features, as these break when LTO is enabled.  It does this by
    automatically defining NO_ACTION_MACRO and NO_ACTION_FUNCTION.  (Note:
    This does not affect QMK Macros and Layers.)

--- keyboards/ferris/keymaps/antuanha/rules.mk
+++ keyboards/ferris/keymaps/antuanha/rules.mk
@@ -1,1 +1,10 @@
 SRC += oneshot.c
+
+# Enables Link Time Optimization (LTO) when compiling the keyboard.  This makes
+# the process take longer, but it can significantly reduce the compiled size
+# (and since the firmware is small, the added time is not noticeable).However,
+# this will automatically disable the legacy TMK Macros and Functions features,
+# as these break when LTO is enabled.  It does this by automatically defining
+# NO_ACTION_MACRO and NO_ACTION_FUNCTION.  (Note: This does not affect QMK
+# Macros and Layers.)
+LTO_ENABLE = yes
